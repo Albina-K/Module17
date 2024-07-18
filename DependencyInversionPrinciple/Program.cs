f21d6b3bb656b5034187a3f26dcac2fde6b43f6b
Almeta-SPO@outlook.com <Almeta-SPO@outlook.com>
2024-07-18 11:32:04 +0300

    end

---
+++ DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace DependencyInversionPrinciple
+{
+    //class VideoAdapter
+    //{
+    //    public string Text { get; set; }
+    //    public Monitor Monitor { get; set; }
+    //    public void Display()
+    //    {
+    //        Monitor.Display(Text);
+    //    }
+    //}
+    //class Monitor
+    //{
+    //    public void Display(string text)
+    //    {
+    //        Console.WriteLine(text);
+    //    }
+    //}
+    ///<summary>
+    ///универсальный интерфейс для вывода изображения
+    /// </summary>
+    public interface IHdmiInterface
+    {
+        void Display(string text);
+    }
+    class Monitor : IHdmiInterface
+    {
+        public void Display(string text)
+        {
+            Console.WriteLine("вывод на монтитор");
+        }
+    }
+    class Tv : IHdmiInterface
+    {
+        public void Display(string text)
+        {
+            Console.WriteLine("Вывод на телевизор");
+        }
+    }
+    ///<summary>
+    ///Адаптер для вывода изображений
+    /// </summary>
+    class VideoAdapter
+    {
+        public string Text { get; set; }
+        public IHdmiInterface HdmiInterface { get; set; }
+        public VideoAdapter(IHdmiInterface hdmiInterface)
+        {
+            HdmiInterface = hdmiInterface;
+        }
+        ///<summary>
+        ///метод вывода
+        /// </summary>
+        public void Display()
+        {
+            HdmiInterface.Display(Text);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //выводит на монитор
+            var connectedMonitor = new VideoAdapter(new Monitor());
+            connectedMonitor.Display();
+
+            //выводим на телевизор
+            var connectedTV = new VideoAdapter(new Tv());
+            connectedTV.Display();
+        }
+    }
+}
---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    /// <summary>
+    /// интерфейс с общим фнкционалом
+    /// </summary>
+    public interface IGeneralPack
+    {
+        void Drive();
+    }
+
+    ///<summary>
+    ///интерфейс подключения офф-роуд систем для внедорожника
+    /// </summary>
+    public interface IOffRoadPack
+    {
+        void DriveDown();
+        void LockDifferential();
+        void DescentAssist();
+    }
+    ///<summary>
+    ///интерфейс подключения систем повышенного комфорта
+    /// </summary>
+    public interface IPremiumPack
+    {
+        void CruiseControl();
+    }
+
+    ///<summary>
+    ///интерфейс подключения спортивных систем
+    /// </summary> 
+    public interface ISportPack
+    {
+        void FourWheelDrive();
+    }
+
+    //Теперь посмотрим, как будет выглядеть софт для седана: 
+    ///<summary>
+    ///у нас премиум седан, поэтому кроме обычных систем подключаем системы комфорта
+    /// </summary>
+    public class Sedane : IGeneralPack, IPremiumPack
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Едем");
+        }
+        public void CruiseControl()
+        {
+            Console.WriteLine("Включаем круиз-контроль");
+        }
+    }
+    //А для внедорожника? 
+
+    ///<summary>
+    ///премиум внедородник
+    ///</summary>
+    public class Suv : IGeneralPack, IPremiumPack, ISportPack, IOffRoadPack
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Едем");
+        }
+        public void CruiseControl()
+        {
+            Console.WriteLine("Включаем полный привод");
+        }
+        public void FourWheelDrive()
+        {
+            Console.WriteLine("Включаем полный привод");
+        }
+        public void DriveDown()
+        {
+            Console.WriteLine("Вкючаем системц помощи при спуске");
+        }
+        public void LockDifferential()
+        {
+            Console.WriteLine("Включаем блокировку центрального дифференциала");
+        }
+        public void DescentAssist()
+        {
+            Console.WriteLine("Включаем систему помощи при спуске");
+        }
+    }
+    //А вот как этим будет пользоваться наш водитель: 
+    public class Driver
+    {
+        public void Drive(IGeneralPack car)
+        {
+            car.Drive();
+        }
+    }
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //наш водитель
+            var driver = new Driver();
+
+            //водителю все равно на чем ехать
+            Console.WriteLine("Садимся в седан");
+            driver.Drive(new Sedane());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Садимся в джип");
+            driver.Drive(new Suv());
+        }
+    }
+}
